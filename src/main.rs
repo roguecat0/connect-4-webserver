0ae5f62b27600b12da10186d3d277a8f089a5e2e
roguecat0 <roguecat0@gmail.com>
2024-11-19 19:01:28 +0100

    semi finished connect4

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{collections::HashMap, ops::IndexMut};
+use connect_4_ai::{OpeningBook, Position, Solver};
+use std::{collections::HashMap, sync::Arc, usize};
 
 use askama::Template;
 use axum::{
@@ -16,6 +17,7 @@ pub enum Cell {
     Red,
     Yellow,
 }
+
 impl Cell {
     pub fn into_html_class(&self) -> &'static str {
         match self {
@@ -42,29 +44,139 @@ async fn main() {
 
 async fn index() -> impl IntoResponse {
     HtmlTemplate(Index {
-        game: GameHtml {
-            values: [[""; 6]; 7],
-            scores: [-4; 7],
-            moves: "".to_string(),
-        },
+        game: GameHtml::new(),
     })
+}
+#[derive(Debug)]
+enum GameStatus {
+    Winning(usize),
+    Losing(usize),
+    Drawing(usize),
+}
+
+impl GameStatus {
+    pub fn new(score: isize, num_moves: usize) -> Self {
+        let state = score.checked_div(score.abs()).unwrap_or(0);
+        println!("div: {:?}, score: {score}", score.checked_div(score.abs()));
+        let moves_left = 21 - num_moves / 2 - score.abs() as usize;
+        println!("state: {state}, num_moves: {num_moves}, m_left: {moves_left}");
+        match (moves_left, state) {
+            (n, 0) => GameStatus::Drawing(n),
+            (n, 1) => GameStatus::Winning(n),
+            (n, -1) => GameStatus::Losing(n),
+            n => panic!("not a possible senario: {n:?}"),
+        }
+    }
+    pub fn reverse(&self) -> Self {
+        match self {
+            GameStatus::Losing(n) => GameStatus::Winning(*n),
+            GameStatus::Drawing(n) => GameStatus::Drawing(*n),
+            GameStatus::Winning(n) => GameStatus::Losing(*n),
+        }
+    }
+    pub fn to_msg(&self) -> String {
+        match self {
+            GameStatus::Losing(0) => format!("You Lost"),
+            GameStatus::Losing(n) => format!("You will lost in {n}, turn(s)"),
+            GameStatus::Drawing(0) => format!("You Drew"),
+            GameStatus::Drawing(n) => format!("You can draw in {n}, turn(s)"),
+            GameStatus::Winning(0) => format!("You Won"),
+            GameStatus::Winning(n) => format!("You can win in {n}, turn(s)"),
+        }
+    }
+    pub fn is_reset(&self) -> bool {
+        match self {
+            GameStatus::Losing(0) | GameStatus::Drawing(0) | GameStatus::Winning(0) => true,
+            _ => false,
+        }
+    }
 }
 
 async fn game(
     Path(moves): Path<String>,
     Query(params): Query<HashMap<String, String>>,
 ) -> impl IntoResponse {
-    let mut game = GameHtml {
-        values: [[""; 6]; 7],
-        scores: [-4; 7],
-        moves: format!("{}{}", &moves[1..], params.get("column").unwrap()),
-    };
+    // check if game end (win, draw)
+    let moves = &moves[1..];
     let pick: usize = params.get("column").unwrap().parse().unwrap();
+    let pos = Position::parse_safe(&moves).unwrap();
+    if pos.is_winning_move(pick) {
+        println!("you won!! {pick}");
+        return HtmlTemplate(GameHtml::from(
+            GameStatus::Winning(0),
+            moves,
+            default_string_array(),
+        ));
+    } else if moves.len() == 41 {
+        println!("you drew!! {pick}");
+        return HtmlTemplate(GameHtml::from(
+            GameStatus::Drawing(0),
+            moves,
+            default_string_array(),
+        ));
+    }
 
-    println!("moves: {}", game.moves);
-    for (ci, col) in game.values.iter_mut().enumerate() {
-        for (i, ii) in game
-            .moves
+    // check if (lost, draw)
+    let moves = format!("{moves}{pick}");
+    let book = Arc::new(OpeningBook::load("7x6.book").unwrap());
+    let mut solver = Solver::with_opening_book(book);
+
+    let position = pos.next_pos_safe(pick);
+    if let None = position {
+        println!("failed to parse moves: {}", moves);
+        return HtmlTemplate(GameHtml::new());
+    }
+    let position = position.unwrap();
+    let scores = solver.analyse(&position, false);
+    println!("scores bot: {scores:?}");
+    let best_move = if let Some(best_move) = get_best_move(&scores) {
+        println!(
+            "you status: {:?}",
+            GameStatus::new(best_move.1, moves.len())
+        );
+        if let GameStatus::Winning(0) | GameStatus::Drawing(0) =
+            GameStatus::new(best_move.1, moves.len())
+        {
+            println!(
+                "you lost whlka!! {} {:?}",
+                moves,
+                GameStatus::new(best_move.1, moves.len())
+            );
+            return HtmlTemplate(GameHtml::from(
+                GameStatus::new(best_move.1, moves.len()).reverse(),
+                &format!("{moves}{}", best_move.0),
+                default_string_array(),
+            ));
+        }
+        best_move
+    } else {
+        println!("bot drew!! {pick}");
+        return HtmlTemplate(GameHtml::from(
+            GameStatus::Drawing(0),
+            &moves,
+            default_string_array(),
+        ));
+    };
+    // coninute evaluating
+    let position = position.next_pos(best_move.0);
+    let moves = format!("{}{}", moves, best_move.0);
+    let scores = solver.analyse(&position, false);
+    println!("scores you: {scores:?}");
+    let game = if let Some(best_move) = get_best_move(&scores) {
+        let status = GameStatus::new(best_move.1, moves.len() - 2);
+        println!("you status: {:?}", status);
+        GameHtml::from(status, &moves, default_string_array())
+    } else {
+        println!("bot drew!! {pick}");
+        GameHtml::from(GameStatus::Drawing(0), &moves, default_string_array())
+    };
+    println!("");
+    HtmlTemplate(game)
+}
+fn get_board_css(moves: &str) -> [[&'static str; 6]; 7] {
+    let mut values = [[""; 6]; 7];
+    for (ci, col) in values.iter_mut().enumerate() {
+        for (i, ii) in moves
             .chars()
             .enumerate()
             .flat_map(|(ii, c)| {
@@ -77,9 +189,47 @@ async fn game(
         }
         col.reverse();
     }
-    HtmlTemplate(game)
+    values
 }
-struct Game;
+fn get_status_message(best_move: (usize, isize), num_moves: usize) -> String {
+    let status = match best_move.1 {
+        0 => "can draw",
+        n if n > 0 => "can win",
+        _ => "will lose",
+    };
+    let moves_left = 21 - num_moves as isize / 2 - best_move.1.abs();
+    format!("You {status} in {moves_left} turn(s)")
+}
+fn get_best_move(scores: &[Option<isize>]) -> Option<(usize, isize)> {
+    scores
+        .into_iter()
+        .enumerate()
+        .flat_map(|(i, c)| c.and_then(|cc| Some((i, cc))))
+        .reduce(|old, new| if old.1 > new.1 { old } else { new })
+}
+
+fn default_string_array() -> [String; 7] {
+    [
+        "None".to_string(),
+        "None".to_string(),
+        "None".to_string(),
+        "None".to_string(),
+        "None".to_string(),
+        "None".to_string(),
+        "None".to_string(),
+    ]
+}
+
+fn transform_scores_array(scores: &[Option<isize>; 7]) -> [String; 7] {
+    fn trans(score: &Option<isize>) -> String {
+        score.map_or("None".to_string(), |d| d.to_string())
+    }
+    let mut output = default_string_array();
+    for (i, score) in scores.into_iter().enumerate() {
+        output[i] = trans(score);
+    }
+    output
+}
 
 #[derive(Template)]
 #[template(path = "index.html")]
@@ -91,8 +241,49 @@ struct Index {
 #[template(path = "game.html")]
 struct GameHtml {
     values: [[&'static str; 6]; 7],
-    scores: [isize; 7],
-    moves: String,
+    scores: [String; 7],
+    path: String,
+    status_msg: String,
+    reset: bool,
+}
+impl GameHtml {
+    pub fn new() -> Self {
+        Self {
+            values: [[""; 6]; 7],
+            scores: default_string_array(),
+            path: "/game/0".to_string(),
+            status_msg: "Game Started".to_string(),
+            reset: false,
+        }
+    }
+    pub fn new_second() -> Self {
+        let mut values = [[""; 6]; 7];
+        values[3][5] = "red";
+        Self {
+            values,
+            scores: default_string_array(),
+            path: "/game/03".to_string(),
+            status_msg: "Game Started".to_string(),
+            reset: false,
+        }
+    }
+
+    pub fn from(status: GameStatus, moves: &str, scores: [String; 7]) -> Self {
+        Self {
+            values: get_board_css(moves),
+            scores,
+            path: Self::generate_path(moves, &status),
+            status_msg: status.to_msg(),
+            reset: status.is_reset(),
+        }
+    }
+    fn generate_path(moves: &str, status: &GameStatus) -> String {
+        if status.is_reset() {
+            "/game/0".to_string()
+        } else {
+            format!("/game/0{}", moves)
+        }
+    }
 }
 
 // render templates
